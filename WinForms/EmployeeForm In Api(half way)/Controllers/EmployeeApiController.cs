1294e5d8502267e31d8b92b24bdd465fcf982e57
aamod0611 <aamod0611@gmail.com>
2017-04-02 20:21:25 +0530

    backup before employeeSummery

---
+++ WinForms/EmployeeForm In Api(half way)/Controllers/EmployeeApiController.cs
@@ -0,0 +1,65 @@
+﻿using EmployeeForm_In_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeForm_In_Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeApiController : ApiController
+    {
+        //EmployeeData data = new EmployeeData();
+
+        EmployeeContext Db = new EmployeeContext();
+
+        [HttpPost]
+        public void PostPersonalDetails([FromBody] EmployeeData data)
+        {
+           Db.employeeData.Add(new EmployeeData { EmployeeName = data.EmployeeName , Email = data.Email, DesignationID = data.DesignationID});
+           Db.SaveChanges();
+            
+        }
+
+        [HttpGet]       
+        public IEnumerable<Designation> GetDesignation()
+        {
+           /* List<Designation> li = Db.designationName.ToList()*/;
+            return Db.designationName.ToList();
+
+        }
+
+        [ActionName("GetRatings")]
+        public IEnumerable<Ratings> GetRatings(int id)
+        {
+            List<Ratings> li = Db.ratings.Where(x => x.SkillTypeID == 1).ToList();            
+            return li;
+        }
+
+        [ActionName("GetSkillNames")]
+        public IEnumerable<Skills> GetSkillNames(int id)
+        {
+            List<Skills> li = Db.skills.Where(x => x.SkillTypeID == 1).ToList();
+            return li;
+
+
+        }
+        [ActionName("GetTechRatings")]
+        public IEnumerable<Ratings> GetTechRatings(int id)
+        {
+            var li = Db.ratings.Where(x => x.SkillTypeID == 2).ToList();
+            return li;
+
+
+        }
+        [ActionName("GetTechSkillNames")]
+        public IEnumerable<Skills> GetTechSkillNames(int id)
+        {
+            var li = Db.skills.Where(x => x.SkillTypeID == 2).ToList();
+            return li;
+        }
+
+    }
+}
---
+++ WinForms/EmployeeForm In Api(half way)/Controllers/EmployeeFormController.cs
@@ -0,0 +1,99 @@
+﻿using EmployeeForm_In_Api.Models;
+using EmployeeForm_In_Api.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeForm_In_Api.Controllers
+{
+
+    public class EmployeeFormController : Controller
+    {
+        int pageCounter;
+        // GET: EmployeeForm
+        EmployeeData data = new EmployeeData();
+        EmployeeContext Db = new EmployeeContext();
+
+        // EmployeeApiController Api = new EmployeeApiController();
+
+        public ActionResult Index()
+        {
+            // Db.employeeData.Add(new EmployeeData { EmployeeName = "hi" });
+            //Db.SaveChanges();
+            //ViewBag.designationList = new SelectList(Api.GetDesignation(), "DesignationID", "DesignationName");
+
+            return View();
+        }
+        public ActionResult Selectskills()
+        {
+            pageCounter = 1;
+            Session["pageCounter"] = 1;
+            return View();
+        }
+
+
+
+
+        public ActionResult DeveloperSkills()
+        {
+
+            //-------------------- either take ratinglist or change the view to get selected radio button ---------
+            EmployeeContext sn = new EmployeeContext();
+            EmployeeViewModel vm = new EmployeeViewModel();
+
+            pageCounter = Convert.ToInt32(Session["pageCounter"]);
+
+            vm.Skillslist = Db.skills.Where(x => x.SkillID == pageCounter).ToList();
+
+            Session["pageCounter"] = ++pageCounter;
+
+
+            int totalCount = sn.skills.Where(x =>x.SkillTypeID==1).Count();
+
+            if (pageCounter > totalCount + 2)
+            {
+                return RedirectToAction("Selectskills");
+            }
+
+            return View(vm);
+
+        }
+
+
+
+
+
+
+        public ActionResult TechnicalSkills()
+        {
+            EmployeeContext sn = new EmployeeContext();
+            EmployeeViewModel vm = new EmployeeViewModel();
+            pageCounter = 6;
+            Session["pageCounter"] = 6;
+
+            pageCounter = Convert.ToInt32(Session["pageCounter"]);
+
+            vm.Skillslist = Db.skills.Where(x => x.SkillID == pageCounter).ToList();
+
+            Session["pageCounter"] = ++pageCounter;
+
+
+            int totalCount = sn.skills.Count();
+
+            if (pageCounter > totalCount + 1)
+            {
+                return RedirectToAction("Selectskills");
+            }
+
+            return View(vm);
+
+        }
+      
+        public ActionResult Summery( )
+        {
+            return View();
+        }
+    }
+}
