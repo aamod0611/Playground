bb3bec0a57943ab44a2ea16707ce2ff4245df07d
aamod0611 <aamod0611@gmail.com>
2017-05-18 18:50:18 +0530

    dont teams and teammembers

---
+++ Django/cci(Teams)/main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from . models import TeamMembers, Teams
+
+admin.site.register(TeamMembers)
+admin.site.register(Teams)
---
+++ Django/cci(Teams)/main/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+
+class TeamMembers(models.Model):
+    Name = models.CharField(max_length = 200)
+    DOB = models.DateField(max_length = 8)
+    Designation = models.CharField(max_length = 200)
+    Pic = models.FileField()
+
+    def get_absolute_url(self):
+        return reverse('main:details', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.Name
+
+
+class Teams(models.Model):
+    TeamName = models.CharField(max_length=20)
+    TeamLead = models.CharField(max_length=20)
+    ClientName = models.CharField(max_length=200)
+    Tech = models.CharField(max_length=200)
+
+    def get_absolute_url(self):
+        return reverse('main:TeamDetails', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.TeamName
